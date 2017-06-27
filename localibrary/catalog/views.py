2cc4d74d2ab9a542d9ca531b5305c9800712521b
dipdebx <deb.dipanjan@gmail.com>
2017-06-27 19:40:59 +0530

    New update

--- localibrary/catalog/views.py
+++ localibrary/catalog/views.py
@@ -25,3 +25,19 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_genre':num_genre,'num_book_word':num_book_word},
     )
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
