edfa91b3c2a933a3ec1ed706d65519950c533072
dipdebx <deb.dipanjan@gmail.com>
2017-07-12 13:56:51 +0530

    Test

--- localibrary/catalog/urls.py
+++ localibrary/catalog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 
-    url(r'^librarian/$', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+    url(r'^borrowed/$', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
     url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
 
     url(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
